79b5ef15d03f5530d66b3eac8e62cbb25a9dad07
dahua777 <1220860737@qq.com>
2016-06-27 09:19:53 +0800

    计算器
    计算器基本完成，最基本的加减乘除可以完成

--- jsq/ViewController.swift
+++ jsq/ViewController.swift
@@ -9,6 +9,101 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func dian(sender: AnyObject) {
+        
+        
+        if(d==0)
+        {
+            sum.text!=sum.text!+"."
+            d=1
+        }
+        if(d==1)
+        {
+            sum.text!=sum.text!
+        }
+        
+    }
+    
+    @IBAction func chu(sender: AnyObject) {
+        flag=4
+        temp=(Double)(sum.text!)!
+        sum.text=""
+        d=0
+    }
+    @IBAction func cheng(sender: AnyObject) {
+        flag=3
+        temp=(Double)(sum.text!)!
+        sum.text=""
+        d=0
+    }
+    @IBAction func jian(sender: AnyObject) {
+        flag=2
+        temp=(Double)(sum.text!)!
+        sum.text=""
+        d=0
+    }
+    @IBAction func dengyu(sender: AnyObject) {
+        if(flag==1)
+        {
+            sum.text="\((Double)(sum.text!)!+temp)"
+            
+            
+        }
+        if(flag==2)
+        {
+            jieguo=temp-(Double)(sum.text!)!
+            sum.text="\(jieguo)"
+        }
+        if(flag==3)
+        {
+            jieguo=temp*(Double)(sum.text!)!
+            sum.text="\(jieguo)"
+        }
+        if(flag==4)
+        {
+            jieguo=temp/(Double)(sum.text!)!
+            sum.text="\(jieguo)"
+        }
+    }
+    var flag=0
+    var temp:Double=0
+    var jieguo:Double=0
+    var d=0
+    @IBAction func add(sender: AnyObject) {
+        flag=1
+        temp=(Double)(sum.text!)!
+        sum.text=""
+        d=0
+        
+    }
+    @IBAction func num9(sender: AnyObject) {
+        sum.text!=sum.text!+"\(9)"
+    }
+    @IBAction func num8(sender: AnyObject) {
+        sum.text!=sum.text!+"\(8)"
+    }
+    @IBAction func num7(sender: AnyObject) {
+        sum.text!=sum.text!+"\(7)"
+    }
+    @IBAction func num6(sender: AnyObject) {
+        sum.text!=sum.text!+"\(6)"
+    }
+    @IBAction func num5(sender: AnyObject) {
+        sum.text!=sum.text!+"\(5)"
+    }
+    @IBAction func num4(sender: AnyObject) {
+        sum.text!=sum.text!+"\(4)"
+    }
+    @IBAction func num3(sender: AnyObject) {
+        sum.text!=sum.text!+"\(3)"
+    }
+    @IBAction func num2(sender: AnyObject) {
+        sum.text!=sum.text!+"\(2)"
+    }
+    @IBAction func num1(sender: AnyObject) {
+        sum.text!=sum.text!+"\(1)"
+    }
+    @IBOutlet weak var sum: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
